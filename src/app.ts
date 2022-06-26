9fb4ee1057d7dc04d6b74c09c1b83e5d8773b77c
Wryth <lucasbriatte@gmail.com>
2022-06-26 10:35:34 +0000

    Updating action

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-let version = "1.0";
+import process from "process";
+process.stdin.resume();
+
+let version = 1.0;
 let message: string = "This is a typescript project.";
 
 console.log(`${version}: ${message}`);
